a2252ce5e18d34e8cda57fb52e26ab0dba5f4e7c
Pavel Bondar <bond.p85@gmail.com>
2017-02-02 15:17:20 +0200

    ImageToServer.xcodeproj -> init project

---
+++ ImageToServer/ImageToServer/AppDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  AppDelegate.swift
+//  ImageToServer
+//
+//  Created by Pavel Bondar on 2/2/17.
+//  Copyright (c) 2017 Pavel Bondar. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+    {
+
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+
+    }
+
+
+}
+
---
+++ ImageToServer/ImageToServer/ImagePeackViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageToServer
+//
+//  Created by Pavel Bondar on 2/2/17.
+//  Copyright (c) 2017 Pavel Bondar. All rights reserved.
+//
+
+import UIKit
+
+class ImagePeackViewController: UIViewController, UIImagePickerControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
+
