219e6d59c8278b8501cf029f485d8feda48ec735
Pavel Bondar <bond.p85@gmail.com>
2017-02-03 11:35:37 +0200

    last with image loader

---
+++ ImageToServer/ImageToServer/Sources/UI/ImagePeackViewController/ImageCollectionViewCell/ImageCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCollectionViewCell.swift
+//  ImageToServer
+//
+//  Created by Pavel Bondar on 2/3/17.
+//  Copyright (c) 2017 Pavel Bondar. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- ImageToServer/ImageToServer/Sources/UI/ImagePeackViewController/ImagePeackViewController.swift
+++ ImageToServer/ImageToServer/Sources/UI/ImagePeackViewController/ImagePeackViewController.swift
@@ -7,20 +7,39 @@
 //
 
 import UIKit
-import AssetsLibrary
+import Photos
 
 class ImagePeackViewController: UIViewController {
-    var library = ALAssetsLibrary()
-    var imageArray = [AnyObject]()
-    var mutableArray = [AnyObject]()
-    let count = 0
+    
+    var images = [UIImage]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
     }
 
-    func allPictures () {
+    func allImages () {
+        let imageManager = PHImageManager.defaultManager()
+        let requestOptions = PHImageRequestOptions()
+        requestOptions.synchronous = false
+        requestOptions.deliveryMode = .HighQualityFormat
         
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        
+        if let fetchResult: PHFetchResult = PHAsset.fetchAssetsWithMediaType(.Image, options: fetchOptions) {
+            if fetchResult.count > 0 {
+                for i in 0..<fetchResult.count {
+                    let img: PHAsset! = fetchResult.objectAtIndex(i) as PHAsset
+                    imageManager.requestImageForAsset(img, targetSize: CGSize(width: 200, height: 200), contentMode: .AspectFill, options: requestOptions, resultHandler: { [weak self] image, error in
+                        image.map{ self?.images.append($0) }
+                    })
+                }
+                
+            } else {
+                print("you have no photos")
+            }
+        }
     }
 }
